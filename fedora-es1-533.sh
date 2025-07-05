ca1257a335e11d5a61873724292390f6b887f2d2
Lotox <70129125+jakjasie1@users.noreply.github.com>
2025-07-05 13:39:02 +0200

    Update fedora-es1-533.sh

--- fedora-es1-533.sh
+++ fedora-es1-533.sh
@@ -5,12 +5,12 @@ root=/dev/sda3
 boot=/dev/sda2
 esp=/dev/sda1
 
-su
-dnf in arch-install-scripts -y
-mount -o subvol=root $root /mnt
-mount $boot /mnt/boot
-mount $esp /mnt/boot/efi
+
+sudo dnf in arch-install-scripts -y
+sudo mount -o subvol=root $root /mnt
+sudo mount $boot /mnt/boot
+sudo mount $esp /mnt/boot/efi
 #will this work?
-arch-chroot /mnt grub2-mkconfig -o /boot/grub2/grub.cfg
-mkdir -p /mnt/boot/efi/EFI/Microsoft/Boot
-cp /mnt/boot/efi/EFI/fedora/grubx64.efi /mnt/boot/efi/EFI/Microsoft/Boot/bootmgfw.efi
+sudo arch-chroot /mnt grub2-mkconfig -o /boot/grub2/grub.cfg
+sudo mkdir -p /mnt/boot/efi/EFI/Microsoft/Boot
+sudo cp /mnt/boot/efi/EFI/fedora/grubx64.efi /mnt/boot/efi/EFI/Microsoft/Boot/bootmgfw.efi
